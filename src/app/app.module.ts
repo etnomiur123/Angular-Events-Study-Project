0ac3ef6493144cec40ea8bc453bd76981fcca886
Rui Monte <CTW01116@criticaltechworks.com>
2021-01-29 19:04:06 +0000

    Fifth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-
+import { ToastrService } from 'ngx-toastr';
 @NgModule({
   bootstrap: [EventsAppComponent],
-  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+  ],
+  providers: [EventService, ToastrService],
   imports: [BrowserModule],
 })
 export class AppModule {}
